7b1a41d123d562972999c08c5b748f983fe778f6
HyogaFukuno <hyouga.dev@gmail.com>
2025-10-21 08:07:56 +0900

    added suspending event node

--- src/main/kotlin/extension/MinestomExtensions.kt
+++ src/main/kotlin/extension/MinestomExtensions.kt
@@ -1,7 +1,14 @@
 package net.orca.extension
 
+import com.github.shynixn.mccoroutine.minestom.addSuspendingListener
+import net.minestom.server.MinecraftServer
 import net.minestom.server.event.Event
 import net.minestom.server.event.EventNode
 
-inline fun <reified T : Event> EventNode<Event>.addListener(noinline function: (T) -> Unit): EventNode<Event> =
+inline fun <reified T : Event> EventNode<in T>.addListener(noinline function: (T) -> Unit): EventNode<in T> =
     addListener(T::class.java) { event -> function(event) }
+
+inline fun <reified T : Event> EventNode<in T>.addListener(server: MinecraftServer, noinline function: suspend (T) -> Unit) {
+    addSuspendingListener(server, T::class.java) { event -> function(event) }
+}
+
